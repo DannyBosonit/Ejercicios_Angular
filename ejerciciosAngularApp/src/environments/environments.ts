a5870561773fe9d0ceb67af1b5efaccfbd946420
DannyBosonit <danielandres.ortiz@bosonit.com>
2024-01-29 09:54:41 -0500

    add exercise 6 covid database

--- ejerciciosAngularApp/src/environments/environments.ts
+++ ejerciciosAngularApp/src/environments/environments.ts
@@ -1,4 +1,5 @@
 export const environments = {
   dataBaseUrl: 'http://localhost:3000',
   countriesUrl: 'https://restcountries.com/v2/all?fields=name',
+  covidUrl: 'https://api.covidtracking.com/v1/states/current.json',
 };
