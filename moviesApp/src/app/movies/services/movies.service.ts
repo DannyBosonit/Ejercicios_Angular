4ef4a397a8999c1510d889a44ebe84cc260ec7bb
DannyBosonit <danielandres.ortiz@bosonit.com>
2024-02-13 10:08:19 -0500

    add params to getMoviesList method

--- moviesApp/src/app/movies/services/movies.service.ts
+++ moviesApp/src/app/movies/services/movies.service.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { MovieList } from '../interfaces/movieList.interface';
+import { Observable, map } from 'rxjs';
+import { Movie, MovieList } from '../interfaces/movieList.interface';
 import { environments } from '../../../environments/environments';
 
 @Injectable({ providedIn: 'root' })
 export class MoviesService {
   private baseUrl: string = environments.baseUrl;
   private apiKey: string = environments.apiKey;
+  private language: string = 'es-ES';
 
   constructor(private http: HttpClient) {}
 
-  getMoviesList(): Observable<MovieList> {
-    return this.http.get<MovieList>(
-      `${this.baseUrl}/movie/now_playing?api_key=${this.apiKey}&language=es-ES`
-    );
+  getMoviesList(): Observable<Movie[]> {
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('language', this.language);
+
+    return this.http
+      .get<MovieList>(`${this.baseUrl}/movie/now_playing`, { params })
+      .pipe(map((resp) => resp.results));
   }
 }
