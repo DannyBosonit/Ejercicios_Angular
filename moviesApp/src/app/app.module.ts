31a09f700e03889da26ac4a318b6727b3556b114
DannyBosonit <danielandres.ortiz@bosonit.com>
2024-02-14 10:52:06 -0500

    install ng-bootstrap for rating component in movies list page

--- moviesApp/src/app/app.module.ts
+++ moviesApp/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgbModule],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
