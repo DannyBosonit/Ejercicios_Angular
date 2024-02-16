4644d0ed01b43a8b438f8f9e78c82be157be774e
DannyBosonit <danielandres.ortiz@bosonit.com>
2024-02-16 14:30:40 -0500

    create movie page functionality

--- moviesApp/src/app/movies/pages/movie-page/movie-page.component.ts
+++ moviesApp/src/app/movies/pages/movie-page/movie-page.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../../services/movies.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MovieDatails } from '../../interfaces/movie.interface';
+import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-movie-page',
   templateUrl: './movie-page.component.html',
-  styles: ``
+  styleUrl: './movie-page.component.css',
 })
-export class MoviePageComponent {
+export class MoviePageComponent implements OnInit {
+  public movie?: MovieDatails;
 
+  constructor(
+    private moviesService: MoviesService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private config: NgbRatingConfig
+  ) {
+    config.max = 10;
+    config.readonly = true;
+  }
+
+  ngOnInit(): void {
+    const { id } = this.activatedRoute.snapshot.params;
+
+    this.moviesService.getMoviesDetails(id).subscribe((movie) => {
+      if (!movie) {
+        this.router.navigate(['/movies/list']);
+        return;
+      }
+
+      this.movie = movie;
+    });
+  }
 }
