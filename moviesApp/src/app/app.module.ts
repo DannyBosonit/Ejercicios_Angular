7dc68abdb2e7e49d70335e1a4569155b0f848636
DannyBosonit <danielandres.ortiz@bosonit.com>
2024-02-16 14:31:05 -0500

    create page component template and styles

--- moviesApp/src/app/app.module.ts
+++ moviesApp/src/app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import localeEsCO from '@angular/common/locales/es-CO';
+import { registerLocaleData } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+registerLocaleData(localeEsCO);
 
 @NgModule({
   declarations: [AppComponent],
